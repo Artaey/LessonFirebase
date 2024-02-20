a3ab176e32ac542163e6e507cc586733c2f0081c
Artaey <95622783+Artaey@users.noreply.github.com>
2024-02-20 11:49:57 +0100

    ...

---
+++ lf/src/configs/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMqOMr6bgah8S-VbxAEht7PobYvZIcTYc",
+  authDomain: "lessonfirebase-307e6.firebaseapp.com",
+  projectId: "lessonfirebase-307e6",
+  storageBucket: "lessonfirebase-307e6.appspot.com",
+  messagingSenderId: "805628494202",
+  appId: "1:805628494202:web:32183bcf29f05aa999d6c3"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
